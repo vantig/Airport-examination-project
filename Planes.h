81593e11bf7017718dea3b943dfb9f981bed50e2
Vanya Gordeychik <vanya.gordejchik@mail.ru>
2020-08-21 02:51:33 +0300

    Добавьте файлы проекта.

---
+++ Planes.h
@@ -0,0 +1,35 @@
+#pragma once
+#include"HeaderMain.h"
+class Plane
+{
+public:
+	Plane()=default;
+	~Plane() = default;
+	std::string	getNumber() const { return number; }
+	std::string	getBrand()const { return brand; }
+	std::string	getReleaseDate()const { return releaseDate; }
+	double getCarrying()const { return carrying; }
+	size_t getPassengersCount()const { return passengersCount; }
+	friend std::istream& operator>>(std::istream& in, Plane&);
+	friend std::ostream& operator<<(std::ostream& out, Plane&);
+private:
+	std::string number;
+	std::string brand;
+	std::string releaseDate;
+	double carrying;
+	size_t passengersCount;
+	
+};
+std::ostream& operator<<(std::ostream& out, const Plane& plane)
+{
+	out << " Plane number " << plane.getNumber() << " brand " << plane.getBrand() << " releaseDate " << plane.getReleaseDate() << " carrying " << plane.getCarrying() << " passengersCount " << plane.getPassengersCount() << std::endl;
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, Plane& plane)
+{
+	in >> plane.number  >> plane.brand >> plane.releaseDate>> plane.carrying >> plane.passengersCount ;
+	return in;
+	
+}
+
