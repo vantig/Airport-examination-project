b73955318375924bfce0796f3da8ce32af24af6c
Vanya Gordeychik <vanya.gordejchik@mail.ru>
2020-08-21 03:17:25 +0300

    class Pilot

---
+++ Pilot.h
@@ -0,0 +1,33 @@
+#pragma once
+#include"HeaderMain.h"
+class Pilot
+{
+public:
+	Pilot() = default;
+	~Pilot() = default;
+	std::string getSurname()const { return surname; }
+	size_t getworkExperienceYears() const { return workExperienceYears; }
+	std::string getAddress()const { return address; }
+	std::string getDateOfBirth()const { return DateOfBirth; }
+	double getSalary()const { return salary; }
+	friend std::istream& operator>>(std::istream& in, Pilot&);
+	friend std::ostream& operator<<(std::ostream& out, Pilot&);
+private:
+	std::string surname;
+	size_t workExperienceYears;
+	std::string address;
+	std::string DateOfBirth;
+	double salary;
+};
+std::ostream& operator<<(std::ostream& out, const Pilot& pilot)
+{
+	out << " Pilot surname " << pilot.getSurname() << " work Experience  " << pilot.getworkExperienceYears() << " Years | address " << pilot.getAddress() << " Date Of Birth " << pilot.getDateOfBirth() << " salary " << pilot.getSalary() << std::endl;
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, Pilot& pilot)
+{
+	in >> pilot.surname >> pilot.workExperienceYears >> pilot.address >> pilot.DateOfBirth >> pilot.salary;
+	return in;
+
+}
